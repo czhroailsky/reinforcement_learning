81383e8ee415b9012a22e44ee539b565bc920020
czhroailsky <javier.reyes.ic@gmail.com>
2019-06-30 22:07:41 -0500

    update

--- deep_q_network_cart_pole/dqn.py
+++ deep_q_network_cart_pole/dqn.py
@@ -1,3 +1,6 @@
+# Source
+# https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+
 import gym 
 import math
 import random
@@ -53,5 +56,17 @@ class ReplayMemory(object):
 
 	def __len__(self):
 		return(len(self.memory))
+
+class DQN(nn.module):
+
+	def __init__(self, h, w, outputs):
+		super(DQN, self).__init__() # Returns a temporary object of the superclass that then allows you to call that superclass's methods
+
+		self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+		self.bn1 = nn.BatchNorm2d(16)
+		self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+		self.bn2 = nn.BatchNorm2d(32)
+		self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+		self.bn3 = nn.BatchNorm2d(32)
 
 
